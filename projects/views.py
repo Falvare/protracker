7a1addf3a6f3da40b1614cce4dde75440f5b4a87
Francisco Alvidrez <falvid16@gmail.com>
2022-07-26 10:35:29 -0600

    Created the projects view and added the url to list of url patterns

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Project, Task
 
 # Create your views here.
+def projects(requests):
+    projects = Project.objects.all
+    return render(requests, 'projects/projects.html', {'projects':projects})
