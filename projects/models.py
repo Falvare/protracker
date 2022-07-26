6912aa089632a1fb9c4f228f9bebf41ba02dbd5e
Francisco Alvidrez <falvid16@gmail.com>
2022-07-25 21:13:48 -0600

    Created the project app models

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    task = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
