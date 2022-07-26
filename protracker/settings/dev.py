c965d62fa7eea06bff3a94f29e99ee1d9e6a0b5c
Francisco Alvidrez <falvid16@gmail.com>
2022-07-25 20:43:02 -0600

    Initial project commit with postgres db

---
+++ protracker/settings/dev.py
@@ -0,0 +1,9 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-ijm5&+rrrhl8nz3*_z9a46quy@n+-^@dk9gumb$kwohy5*bbhs'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
---
+++ protracker/settings/prod.py
@@ -0,0 +1,10 @@
+from .base import *
+import os
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['localhost', 'dj-protracker.herokuapp.com']
