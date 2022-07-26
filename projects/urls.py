a8cf131a2896d9accff46eea46d94ff1da036549
Francisco Alvidrez <falvid16@gmail.com>
2022-07-26 12:03:29 -0600

    Added the add task url to url patterns

--- projects/urls.py
+++ projects/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path 
-from .views import projects, createProject, projectDetails
+from .views import projects, createProject, projectDetails, addTask
 
 urlpatterns = [
     path('', projects, name='projects'),
     path('projects/', projects, name='projects'),
     path('create-project/', createProject, name='create project'),
-    path('project-details/<int:pk>', projectDetails, name='project details'),
+    path('projects/<int:pk>', projectDetails, name='project details'),
+    path('projects/<int:pk>/add-task', addTask, name='add task'),
 ]
