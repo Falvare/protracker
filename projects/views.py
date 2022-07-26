e3a9374d43f8af84a641c21ed7f51ef86e4aa3ba
Francisco Alvidrez <falvid16@gmail.com>
2022-07-26 12:02:35 -0600

    Created the add task view and changed project details view to include tasks related to project

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, reverse
 from .models import Project, Task
 from .forms import ProjectForm, TaskForm
 
@@ -20,4 +20,23 @@ def createProject(requests):
 
 def projectDetails(requests, pk):
     project = Project.objects.get(pk=pk)
-    return render(requests, 'projects/project_details.html', {'project':project})
+    tasks = Task.objects.filter(project=project)
+
+    context = {
+        'project':project,
+        'tasks':tasks
+        }
+    return render(requests, 'projects/project_details.html', context=context)
+
+def addTask(requests, pk):
+    if requests.method == 'POST':
+        form = TaskForm(requests.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.project = Project.objects.get(pk=pk)
+            task.save()
+            return redirect('/projects/' + str(pk))
+    else:
+        form = TaskForm()
+
+    return render(requests, 'projects/add_task.html', {'form':form})
